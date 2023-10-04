b713931615cfd20f468578fac225613065c30891
GuillermoRached <guillermorached@hotmail.com>
2023-10-04 09:59:07 -0400

    feat: add loopcheck basic example & modify script

---
+++ c_tests/loop_check/guillermo_examples/asm/arm/simple_secure.s
@@ -0,0 +1,78 @@
+	.arch armv5t
+	.eabi_attribute 20, 1
+	.eabi_attribute 21, 1
+	.eabi_attribute 23, 3
+	.eabi_attribute 24, 1
+	.eabi_attribute 25, 1
+	.eabi_attribute 26, 2
+	.eabi_attribute 30, 6
+	.eabi_attribute 34, 0
+	.eabi_attribute 18, 4
+	.file	"simple_secure.c"
+	.text
+	.align	2
+	.global	faultDetect
+	.syntax unified
+	.arm
+	.fpu softvfp
+	.type	faultDetect, %function
+faultDetect:
+	@ args = 0, pretend = 0, frame = 0
+	@ frame_needed = 1, uses_anonymous_args = 0
+	@ link register save eliminated.
+	str	fp, [sp, #-4]!
+	add	fp, sp, #0
+	nop
+	add	sp, fp, #0
+	@ sp needed
+	ldr	fp, [sp], #4
+	bx	lr
+	.size	faultDetect, .-faultDetect
+	.section	.rodata
+	.align	2
+.LC0:
+	.ascii	"i = %d\012\000"
+	.text
+	.align	2
+	.global	main
+	.syntax unified
+	.arm
+	.fpu softvfp
+	.type	main, %function
+main:
+	@ args = 0, pretend = 0, frame = 8
+	@ frame_needed = 1, uses_anonymous_args = 0
+	push	{fp, lr}
+	add	fp, sp, #4
+	sub	sp, sp, #8
+	mov	r3, #0
+	str	r3, [fp, #-8]
+	b	.L3
+.L4:
+	ldr	r1, [fp, #-8]
+	ldr	r0, .L7
+	bl	printf
+	ldr	r3, [fp, #-8]
+	add	r3, r3, #1
+	str	r3, [fp, #-8]
+.L3:
+	ldr	r3, [fp, #-8]
+	cmp	r3, #9
+	ble	.L4
+	ldr	r3, [fp, #-8]
+	cmp	r3, #9
+	bgt	.L5
+	bl	faultDetect
+.L5:
+	mov	r3, #0
+	mov	r0, r3
+	sub	sp, fp, #4
+	@ sp needed
+	pop	{fp, pc}
+.L8:
+	.align	2
+.L7:
+	.word	.LC0
+	.size	main, .-main
+	.ident	"GCC: (Ubuntu 9.4.0-1ubuntu1~20.04.2) 9.4.0"
+	.section	.note.GNU-stack,"",%progbits
