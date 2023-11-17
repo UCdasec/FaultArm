c99435a143006c1d1fbb33d19821234d6930b83c
GuillermoRached <guillermorached@hotmail.com>
2023-11-17 09:46:55 -0500

    feat: add bash script and preliminary results

--- scripts/run_on_all.sh
+++ scripts/run_on_all.sh
@@ -1,25 +1,22 @@
 #!/bin/bash
 
-# Check for correct number of arguments
-if [ "$#" -ne 3 ]; then
-    echo "Usage: $0 <path_to_python_program> <source_directory> <output_directory>"
+# Check if a directory is provided
+if [ $# -lt 2 ]; then
+    echo "Usage: $0 <source_directory> <output_file_name>"
     exit 1
 fi
 
-PYTHON_PROGRAM="$1"
-SOURCE_DIR="$2"
-OUTPUT_DIR="$3"
+DIRECTORY=$1
+OUTPUT_FILE_NAME=$2
 
-# Create the output directory if it doesn't exist
-mkdir -p "$OUTPUT_DIR"
-
-# Find all .s files recursively and run the python program on them
-find "$SOURCE_DIR" -type f -name "*.s" | while read file; do
-    # Get a unique output filename by replacing slashes with underscores
-    OUTPUT_FILE=$(echo "$file" | sed 's/^\.\///;s/\//_/g')
-    
-    # Run the Python program and save the output
-    python "$PYTHON_PROGRAM" "$file" > "$OUTPUT_DIR/$OUTPUT_FILE.out"
+# Loop through each .s file in the directory
+for file in "$DIRECTORY"/*.s; do
+    # Check if file exists
+    if [ -f "$file" ]; then
+        echo "Processing $file..."
+        # Run the Python program and append the output to output.txt
+        python main.py "$file" >> $OUTPUT_FILE_NAME.txt
+    fi
 done
 
-echo "Processing complete!"
+echo "Processing complete. Output saved in $OUTPUT_FILE_NAME.txt."
