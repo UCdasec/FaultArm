73a3c0357dec865b05cdc2755802939df29085da
kharanpv <kharanpv@mail.uc.edu>
2024-02-06 13:45:09 -0500

    modified shell scripts and deleted spreadsheet analysis builder because defunct

--- scripts/print_all_output.sh
+++ scripts/print_all_output.sh
@@ -1,22 +1,25 @@
 #!/bin/bash
 
 # Check if a directory is provided
-if [ $# -lt 2 ]; then
-    echo "Usage: $0 <source_directory> <output_file_name>"
+if [ $# -lt 1 ]; then
+    echo "Usage: $0 <source_directory>"
     exit 1
 fi
 
-DIRECTORY=$1
-OUTPUT_FILE_NAME=$2
+# Resolve absolute path of the script directory
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+# Source directory
+SOURCE_DIRECTORY="$1"
+
+# Output file name
+OUTPUT_FILE="../out/output.txt"
 
 # Loop through each .s file in the directory
-for file in "$DIRECTORY"/*.s; do
-    # Check if file exists
-    if [ -f "$file" ]; then
-        echo "Processing $file..."
-        # Run the Python program and append the output to output.txt
-        python main.py "$file" >> $OUTPUT_FILE_NAME.txt
-    fi
+find "$SOURCE_DIRECTORY" -type f -name '*.s' | while read -r file; do
+    # Run the Python program and append the output to output.txt
+    echo "Processing $file..."
+    python3 "../main.py" "$file" >> "$OUTPUT_FILE"
 done
 
-echo "Processing complete. Output saved in $OUTPUT_FILE_NAME.txt."
+echo "Processing complete. Output saved in $OUTPUT_FILE."
